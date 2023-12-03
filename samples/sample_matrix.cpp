3a2da0114125f892214121a5ec207dc28a39b845
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2023-12-03 21:51:38 +0300

    all done

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -6,14 +6,25 @@
 
 #include <iostream>
 #include "tmatrix.h"
-#include <Windows.h>
 //---------------------------------------------------------------------------
-
-#pragma comment(lib, "user32.lib")
 
 void main()
 {
-	keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY, 0);
-	keybd_event(VK_NUMLOCK, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    TDynamicMatrix<int> a(5), b(5), c(5);
+    int i, j;
+
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование класс работы с матрицами"
+        << endl;
+    for (i = 0; i < 5; i++)
+        for (j = i; j < 5; j++)
+        {
+            a[i][j] = i * 10 + j;
+            b[i][j] = (i * 10 + j) * 100;
+        }
+    c = a + b;
+    cout << "Matrix a = " << endl << a << endl;
+    cout << "Matrix b = " << endl << b << endl;
+    cout << "Matrix c = a + b" << endl << c << endl;
 }
 //---------------------------------------------------------------------------
